5699fa475ee9a30ff4562f479c22336382798201
Syed Maqsood <maq9713@gmail.com>
2017-02-26 16:34:23 +0400

    down function defined

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -3,10 +3,11 @@
 #include <ctime>
 using namespace std;
 void display(int p[3][3]);
+void d(int p[3][3]);
 
 int main()
 {
-    int i, j, s, a[3][3]={{0,8,9},{7,8,9},{7,8,9}};
+    int i, j, s, a[3][3]={{0,8,9},{7,7,9},{7,8,5}};
     char inp;
 display(a);
 
@@ -58,6 +59,8 @@ display(a);
     }
     else if((inp=='D' || inp=='d' )){
         cout<<"d func will be executed\n"<<endl;
+        d(a);
+
     }
     else{
         cout<<"l func will be executed\n"<<endl;
@@ -68,6 +71,24 @@ display(a);
     return 0;
 }
 
+void d(int p[3][3]){
+        for(int j=0; j<=2; j++){
+            int i=0;
+            if(p[i+2][j]==p[i+1][j]){
+                p[i+2][j]=p[i+2][j]*2;
+                p[i+1][j]=p[i][j];
+                p[i][j]=0;
+            }
+            else if(p[i+1][j]==p[i][j]){
+                p[i+1][j]=p[i+1][j]*2;
+                p[i][j]=0;
+            }
+
+        }
+        cout<<"disp"<<endl;
+        display(p);
+}
+
 void display(int p[3][3]){
     for(int i=0; i<=2; i++){
         for(int j=0; j<=2; j++){
@@ -76,3 +97,4 @@ void display(int p[3][3]){
         cout<<endl;
     }
 }
+
