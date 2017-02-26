bc869ab40b5421f798bb3f8d9449f405f9c2cbd5
Syed Maqsood <maq9713@gmail.com>
2017-02-26 21:07:36 +0400

    2048 completed.

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -9,18 +9,16 @@ int main()
 {
     int i, j, s, a[3][3]={{0,0,0},{0,2,0},{0,0,0}};
     char inp;
-display(a);
 
+    cout<<"Directions to use:- \n U-->Up \n R-->Right \n D-->Down \n L-->Left\n"<<endl;
+    _sleep(3000);
+    system("cls");
 
     //Step1: Insert 2 at Random place
     step1: srand(time(0));
     s=(rand()%9)/3;
     i=s/3; //Column value
     j=s%3; //Row value
-    if(1){
-        cout<<"jhj"<<s<<i << j<<endl;
-    }
-
     for(int q=0; q<=2; q++)
     {
         for(int p=0; p<=2; p++)
@@ -37,36 +35,27 @@ display(a);
     }
 
     //Step2: Display
-    step2: display(a);
-    cout<<"\nDirection:- \n U-->Up \n R-->Right \n D-->Down \n L-->Left\n"<<endl;
+    step2: system("cls");
+    display(a);
 
     //Step3: Input decision making
-    cin >> inp;
+    step3: cin >> inp;
     if(!(inp=='U' || inp=='R' || inp=='D' || inp=='L' || inp=='r' || inp=='u' || inp=='d' || inp=='l' )){
-        system("cls");
-        cout<<"Enter a valid input\n"<<endl;
-        goto step2;
+        cout<<"!!Enter a valid input!!\n"<<endl;
+        goto step3;
     }
     system("cls");
     display(a);
 
     //Step4: Decision function
-    if((inp=='U' || inp=='u' )){
-        cout<<"up func will be executed\n"<<endl;
+    if((inp=='U' || inp=='u' ))
         mov_func(a,2,1,0,0,0,0,0);
-    }
-    else if((inp=='R' || inp=='r' )){
-        cout<<"r func will be executed\n"<<endl;
+    else if((inp=='R' || inp=='r' ))
         mov_func(a,0,0,0,0,1,2,1);
-    }
-    else if((inp=='D' || inp=='d' )){
-        cout<<"d func will be executed\n"<<endl;
+    else if((inp=='D' || inp=='d' ))
         mov_func(a,0,1,2,0,0,0,0);
-    }
-    else{
-        cout<<"l func will be executed\n"<<endl;
+    else
         mov_func(a,0,0,0,2,1,0,1);
-    }
 
     //Step5: Exit condition
     for(int i=0; i<=2; i++){
@@ -77,65 +66,10 @@ display(a);
         }
     }
     cout<<"\nGame over";
+    cout<<"\n\n\t-by SYED MAQSOOD\n\n";
+
+    _sleep(3000);
     return 0;
-}
-
-void mov_func(int p[3][3],int i0,int i1,int i2,int j0,int j1,int j2,bool sw){
-        for(int j=0; j<=2; j++){
-            int i=0;
-            if(sw==1){
-                int temp=i;
-                i=j;
-                j=temp;
-            }
-
-            //addition comnditions
-            if(p[i+i2][j+j2]==p[i+i1][j+j1]){
-                p[i+i2][j+j2]=p[i+i2][j+j2]*2;
-                p[i+i1][j+j1]=p[i+i0][j+j0];
-                p[i+i0][j+j0]=0;
-                cout<<" c1" <<endl;
-            }
-            else if(p[i+i1][j+j1]==p[i+i0][j+j0]){
-                p[i+i1][j+j1]=p[i+i1][j+j1]*2;
-                p[i+i0][j+j0]=0;
-                cout<<" c2" <<endl;
-            }
-            if(p[i+i1][j+j1]==0){
-                if(p[i+i2][j+j2]==p[i+i0][j+j0]){
-                p[i+i2][j+j2]=p[i+i2][j+j2]*2;
-                p[i+i0][j+j0]=0;
-                cout<<" c1" <<endl;
-                }
-            }
-
-
-            //moving conditions
-            if(p[i+i2][j+j2]==0){
-                if(p[i+i1][j+j1]==0){
-                    p[i+i1][j+j1]= p[i+i0][j+j0];
-                    p[i+i0][j+j0]=0;
-                }
-                p[i+i2][j+j2]= p[i+i1][j+j1];
-                p[i+i1][j+j1]=p[i+i0][j+j0];
-                p[i+i0][j+j0]=0;
-                cout<<" c1dgdf" <<endl;
-            }
-            else if(p[i+i1][j+j1]==0){
-                p[i+i1][j+j1]=p[i+i0][j+j0];
-                p[i+i0][j+j0]=0;
-                cout<<" c2" <<endl;
-            }
-            cout<<" c2" <<endl;
-
-
-            if(sw==1){
-                int temp=i;
-                i=j;
-                j=temp;
-            }
-        }
-        display(p);
 }
 
 void display(int p[3][3]){
@@ -147,3 +81,51 @@ void display(int p[3][3]){
     }
 }
 
+void mov_func(int p[3][3],int i0,int i1,int i2,int j0,int j1,int j2,bool sw){
+    for(int j=0; j<=2; j++){
+        int i=0;
+        if(sw==1){
+            int temp=i;
+            i=j;
+            j=temp;
+        }
+
+        //addition comnditions
+        if(p[i+i2][j+j2]==p[i+i1][j+j1]){
+            p[i+i2][j+j2]=p[i+i2][j+j2]*2;
+            p[i+i1][j+j1]=p[i+i0][j+j0];
+            p[i+i0][j+j0]=0;
+        }
+        else if(p[i+i1][j+j1]==p[i+i0][j+j0]){
+            p[i+i1][j+j1]=p[i+i1][j+j1]*2;
+            p[i+i0][j+j0]=0;
+        }
+        if(p[i+i1][j+j1]==0){
+            if(p[i+i2][j+j2]==p[i+i0][j+j0]){
+                p[i+i2][j+j2]=p[i+i2][j+j2]*2;
+                p[i+i0][j+j0]=0;
+            }
+        }
+
+        //moving conditions
+        if(p[i+i2][j+j2]==0){
+            if(p[i+i1][j+j1]==0){
+                p[i+i1][j+j1]= p[i+i0][j+j0];
+                p[i+i0][j+j0]=0;
+            }
+            p[i+i2][j+j2]= p[i+i1][j+j1];
+            p[i+i1][j+j1]=p[i+i0][j+j0];
+            p[i+i0][j+j0]=0;
+        }
+        else if(p[i+i1][j+j1]==0){
+            p[i+i1][j+j1]=p[i+i0][j+j0];
+            p[i+i0][j+j0]=0;
+        }
+
+        if(sw==1){
+            int temp=i;
+            i=j;
+            j=temp;
+        }
+    }
+}
